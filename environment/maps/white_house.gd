5d7d4daf1bf58b130c1f0d4e2045344e1f323307
Max Royer <mextie@macbook-pro-36.rollins.edu>
2025-01-21 00:09:56 -0500

    added the basics of the fadeout from the house scene

--- environment/maps/white_house.gd
+++ environment/maps/white_house.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @onready var world_environment : WorldEnvironment = $WorldEnvironment
+@onready var player: CharacterBody3D = $Player
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,4 +13,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body == player:
+		await get_tree().create_timer(60).timeout
+		exitScene()
+
+func exitScene():
+	var tween = get_tree().create_tween()
+	
+	tween.tween_property(world_environment.environment, "fog_depth_end", 0.1, 5)
+	
 	pass
